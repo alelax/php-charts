b560e439360d093deacd436c86d9ae5eacc3b92e
alelax <alessandro.lausdei@gmail.com>
2018-05-18 09:24:43 +0200

    php-charts - second step - ok

--- data.php
+++ data.php
@@ -2,3 +2,23 @@
   $data = [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322];
   $labels = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
 ?>
+
+
+<?php
+  $graphs = [
+    'fatturato' => [
+        'type' => ['line'],
+        'data' => [1000,1322,1123,2301,3288,988,502,2300,5332,2300,1233,2322],
+        'labels' => ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"]
+    ],
+    'fatturato_by_agent' => [
+      'type' => ['pie'],
+      'data' => [
+        'Marco' => 9000,
+        'Giuseppe' => 4000,
+        'Mattia' => 3200,
+        'Alberto' => 2300
+     ],
+    ]
+  ];
+ ?>
--- index.php
+++ index.php
@@ -11,25 +11,130 @@
 
       <main>
 
+
          <div class="chart-container">
-            <canvas id="annual-sales" width="400" height="400"></canvas>
+            <canvas id="annual-sales"></canvas>
+         </div>
+
+         <div class="chart-container">
+            <canvas id="monthly-sales"></canvas>
          </div>
 
       </main>
+
+
+
 
       <script type="text/javascript">
 
          <?php include 'data.php' ?>
 
          $(document).ready(function(){
-            var chart_context = $('#annual-sales');
 
-            var chart_data = <?php echo json_encode($data) ?>;
-            var chart_labels = <?php echo json_encode($labels) ?>;
+            <?php include 'data.php' ?>
 
-            var myChart = new Chart( chart_context, chart_config('line', chart_labels, chart_data, 'Vendite Mensili', 'rgba(22, 59, 213, 0.65)', 'rgba(0, 37, 190, 1)' ) );
+            //First point
+            // var cxt_line_chart = $('#annual-sales');
+            // var line_chart_data = <?php echo json_encode($data) ?>;
+            // var line_chart_labels = <?php echo json_encode($labels) ?>;
+            // var line_chart = new Chart( cxt_line_chart, chart_config('line', line_chart_labels, line_chart_data, 'Vendite Mensili', 'rgba(22, 59, 213, 0.65)', 'rgba(0, 37, 190, 1)' ) );
 
 
+            //Second point
+            <?php
+
+               foreach ($graphs as $key => $graph) {
+
+                  if ($key == 'fatturato') {
+
+                     foreach ($graph as $f_key => $f_value) {
+
+                        if ($f_key == 'type') {
+                           $line_type = $f_value;
+                        } elseif ($f_key == 'data') {
+                           $line_data = $f_value;
+                        } elseif ($f_key == 'labels') {
+                           $line_labels = $f_value;
+                        }
+
+                     }
+                  }
+
+                  elseif ($key == 'fatturato_by_agent') {
+
+                     foreach ($graph as $k => $value) {
+
+                        if ($k == 'type') {
+                           $pie_type = $value;
+                        }
+
+                        elseif ($k == 'data') {
+                           foreach ($value as $agent => $amount) {
+                              $pie_labels[] = $agent;
+                              $pie_data[] = $amount;
+                           }
+                        }
+
+                     }
+                  }
+               }
+
+            ?>
+
+            var cxt_line_chart2 = $('#annual-sales');
+
+            var line_chart_type2 = <?php echo json_encode($line_type) ?>;
+            console.log('line type');
+            console.log(line_chart_type2);
+
+            var line_chart_data2 = <?php echo json_encode($line_data) ?>;
+            console.log('line data');
+            console.log(line_chart_data2);
+
+            var line_chart_labels2 = <?php echo json_encode($line_labels) ?>;
+            console.log('line label');
+            console.log(line_chart_labels2);
+
+            var line_chart = new Chart( cxt_line_chart2, chart_config(line_chart_type2, line_chart_labels2, line_chart_data2, 'Vendite Mensili', 'rgba(4, 214, 0, 0.65)', 'rgba(0, 37, 190, 1)' ) );
+
+            var cxt_pie_chart = $('#monthly-sales');
+
+            var pie_chart_type2 = <?php echo json_encode($pie_type) ?>;
+            console.log('pie type');
+            console.log(pie_chart_type2);
+
+            var pie_chart_data2 = <?php echo json_encode($pie_data) ?>;
+            console.log('pie data');
+            console.log(pie_chart_data2);
+
+            var pie_chart_labels2 = <?php echo json_encode($pie_labels) ?>;
+            console.log('pie label');
+            console.log(pie_chart_labels2);
+
+            var line_chart = new Chart( cxt_pie_chart, chart_config(pie_chart_type2, pie_chart_labels2, pie_chart_data2, 'Vendite Individuali', 'rgba(4, 214, 0, 0.65)', 'rgba(0, 37, 190, 1)' ) );
+
+
+
+
+            //<?php
+            //
+            //    $lab = [];
+            //    $amt = [];
+            //    foreach ($graphs['fatturato_by_agent'] as $key => $graph) {
+            //       if ($key == 'data') {
+            //          foreach ($graph as $k => $element) {
+            //             var_dump($element);
+            //             // $lab[] = $k;
+            //             // $amt[] = $element;
+            //          }
+            //       }
+            //    };
+            //
+            //    var_dump($lab);die();
+            //    // var_dump('---------');
+               // var_dump($amt);
+
+            ?>
 
 
 
@@ -37,23 +142,23 @@
             //Crea e restituisce l'oggetto richiesto da Chart.js per creare il grafico
             function chart_config(chart_type, chart_labels, chart_data, chart_title, mainColor, secondaryColor ) {
 
-            var c_config = {
+               var c_config = {
 
-               type: chart_type,
-               data: {
-                  labels: chart_labels,
-                  datasets: [{
-                     label: chart_title,
-                     data: chart_data,
-                     backgroundColor: mainColor,
-                     borderColor: secondaryColor,
-                  }]
-               }
+                  type: chart_type,
+                  data: {
+                     labels: chart_labels,
+                     datasets: [{
+                        label: chart_title,
+                        data: chart_data,
+                        backgroundColor: mainColor,
+                        borderColor: secondaryColor,
+                     }]
+                  }
 
-            };
+               };
 
-            return c_config;
-         }
+               return c_config;
+            }
 
          });
 
